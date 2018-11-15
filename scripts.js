44dbd3eb0f691b081cb289c4fbd05072b4178152
tzits <tzitsman@gmail.com>
2018-11-15 17:10:35 -0500

    removing no longer used code

--- scripts.js
+++ scripts.js
@@ -35,15 +35,11 @@ createCard = function() {
 		})
 	}
 	var k = words.length
-	const newWords = words
-	console.log(newWords)
 	for(var i=0 ; i < k; i ++) {
 		j = Math.floor(Math.random() * (words.length));
 		var card = new Card();
-		console.log(words[j])
 		card.create(words[j].key,words[j].number);
 		words.splice(j,1)
-		console.log(newWords)
 	}
 	$('button').remove()
 }
